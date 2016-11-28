19a8568e57c5333c417bc66b73d7a1177ea69409
RyannDaGreat <ryancentralorg@gmail.com>
2016-11-28 05:32:30 -0500

    ssf

--- HelperClass.java
+++ HelperClass.java
@@ -9,7 +9,7 @@ public class HelperClass
         System.out.println("HelperClass works!");
     }
     public static void main(String[] args) throws InterruptedException, ClassNotFoundException, InstantiationException, IllegalAccessException, IOException
-    {rAutoUpdater.main(new String[]{});
+    {
         r.say("Hello how are you today good fine sir on a unicycle? Or a motorcycle? Or how about just a plain old donkey kongkey? This is wonderful...I wonder If i can send this across computers easily?");
     }
 }
