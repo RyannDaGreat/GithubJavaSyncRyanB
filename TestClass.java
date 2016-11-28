94d21b056c5c298ed55b3d3c52628ae01558d236
RyannDaGreat <ryancentralorg@gmail.com>
2016-11-28 05:42:34 -0500

    ssf

--- TestClass.java
+++ TestClass.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 public class TestClass
 {
-    public static void main(String... args) throws InterruptedException, ClassNotFoundException, InstantiationException, IOException, IllegalAccessException
+    public static void main(String... args) throws  ClassNotFoundException, InstantiationException, IOException, IllegalAccessException
     {
         System.out.println(System.getProperty("user.home") + "/Desktop");
         System.out.println("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
