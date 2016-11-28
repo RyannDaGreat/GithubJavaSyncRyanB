fdb3179f1a7cfcf55e87f471222d9b474ddaff15
RyannDaGreat <ryancentralorg@gmail.com>
2016-11-28 04:16:41 -0500

    ssf

---
+++ HelperClass.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Ryan on 11/28/16.
+ */
+public class HelperClass
+{
+    static
+    {
+        System.out.println("HelperClass works!");
+    }
+    public static void main(String[] args)
+    {
+    }
+}
--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,8 @@ public class TestClass
 {
     public static void main(String... args)
     {
-        System.out.println("Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
+        System.out.println(System.getProperty("user.home") + "/Desktop");
+        System.out.println("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
+        HelperClass.main(new String[]{});
     }
 }
