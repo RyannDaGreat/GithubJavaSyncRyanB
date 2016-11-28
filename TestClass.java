cbbddef53c4e6bdb48c08edd3e0843dc810392e4
RyannDaGreat <ryancentralorg@gmail.com>
2016-11-28 02:40:56 -0500

    ssf

--- TestClass.java
+++ TestClass.java
@@ -1,6 +1,10 @@
 public class TestClass
 {
-    public static void main(String[] args)
+    static
+    {
+        main();
+    }
+    public static void main(String... args)
     {
         System.out.println("SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
     }
