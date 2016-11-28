c0ceb6ae2006f0cdc67787eb823eca06a35df35d
RyannDaGreat <ryancentralorg@gmail.com>
2016-11-28 05:33:20 -0500

    ssf

--- TestClass.java
+++ TestClass.java
@@ -5,6 +5,5 @@ public class TestClass
     {
         System.out.println(System.getProperty("user.home") + "/Desktop");
         System.out.println("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
-        HelperClass.main(new String[]{});
     }
 }
