fefee2e38ff1665f9591f1a79ed3ad9a69a4bab3
RyannDaGreat <ryancentralorg@gmail.com>
2016-11-28 01:18:12 -0500

    Initial commit

---
+++ TestClass.java
@@ -0,0 +1,7 @@
+public class TestClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
+    }
+}
