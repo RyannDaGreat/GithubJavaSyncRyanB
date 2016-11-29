8124d9c7d5444e9e85e10694b22e61c1d473a80f
RyannDaGreat <ryancentralorg@gmail.com>
2016-11-29 13:25:09 -0500

    ssf

---
+++ FinallyBlockTester.java
@@ -0,0 +1,22 @@
+//region a
+@SuppressWarnings("ReturnInsideFinallyBlock")
+public class FinallyBlockTester
+{
+
+    public static String post_print()
+    {
+        try
+        {
+            return "Hello";
+        }
+        finally
+        {
+            System.out.println("haHA! If this message prints last, I can do something AFTER you've run me! UPDATE: Apparently not :( Why?!");
+            return "Goodbye";
+        }
+    }
+    public static void main(String[] args)
+    {
+        System.out.println(post_print());
+    }
+}
