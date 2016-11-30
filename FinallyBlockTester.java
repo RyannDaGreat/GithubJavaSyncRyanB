da63bf1bf4d9046bf250e2c07280a033a3b69502
RyannDaGreat <ryancentralorg@gmail.com>
2016-11-30 17:16:56 -0500

    ssf

--- FinallyBlockTester.java
+++ FinallyBlockTester.java
@@ -17,6 +17,7 @@ public class FinallyBlockTester
     }
     public static void main(String[] args)
     {
+
         System.out.println(post_print());
     }
 }
--- TestClass.java
+++ TestClass.java
@@ -6,5 +6,10 @@ public class TestClass
         System.out.println(System.getProperty("user.home") + "/Desktop");
         r.say("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
         System.out.println("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
+        String out=r.scan("Enter a string in the console: ");
+        for(int i=0;i<out.length();i++)
+        {
+            System.out.println(out.substring(i));
+        }
     }
 }
