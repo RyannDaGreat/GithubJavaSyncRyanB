6b1b8a0991dfdd08cfd3952776e020e0c04da5ac
RyannDaGreat <ryancentralorg@gmail.com>
2017-01-24 01:34:35 -0500

    ssf

--- TestClass.java
+++ TestClass.java
@@ -7,6 +7,7 @@ public class TestClass
 //        r.say("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
         System.out.println("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
 //        String out=r.scan("Enter a string in the console: ");
-        r.say("HELLO ARE YOU A HUMAN");
+        r.say("I've just changed the source code you're running right now.");
+        System.out.println("[Insert troll face here]");
     }
 }
