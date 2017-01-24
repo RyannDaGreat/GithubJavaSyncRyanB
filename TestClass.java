37dbcf2b8279d9b903a6f826b3f26d9bfb168171
RyannDaGreat <ryancentralorg@gmail.com>
2017-01-24 01:21:57 -0500

    ssf

--- TestClass.java
+++ TestClass.java
@@ -7,5 +7,6 @@ public class TestClass
 //        r.say("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
         System.out.println("WOOT WOOT WOOT WOOT Now for an auto-update test....SUCCESS!!!...If you're reading this from a terminal, then we're up and running.");
 //        String out=r.scan("Enter a string in the console: ");
+        r.say("HELLO ARE YOU A HUMAN");
     }
 }
